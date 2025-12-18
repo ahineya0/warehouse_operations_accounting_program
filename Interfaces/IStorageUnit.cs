242d0cd0524c46f6e0d5b605c4758c2528d8c9e8
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 14:02:18 +0700

    Changed StorageUnit logic

--- Interfaces/IStorageUnit.cs
+++ Interfaces/IStorageUnit.cs
@@ -7,10 +7,6 @@ namespace warehouse_operations_accounting_program.Interfaces
 {
     public interface IStorageUnit
     {
-        int Id { get; }
-        decimal Capacity { get; }
-        decimal Occupied { get; }
-
         bool CanStore(Goods goods);
         void AddGoods(Goods goods);
         void RemoveGoods(Goods goods);
--- Models/StorageUnit.cs
+++ Models/StorageUnit.cs
@@ -5,15 +5,22 @@ using warehouse_operations_accounting_program.Interfaces;
 
 namespace warehouse_operations_accounting_program.Models
 {
-    public abstract class StorageUnit : IStorageUnit
+    public class StorageUnit : IStorageUnit
     {
         protected readonly List<Goods> goods = new();
         public int Id { get; set; }
-        public abstract decimal Capacity { get; }
-        public abstract decimal Occupied { get; }
+        public decimal AreaCapacity { get; set; }
+        public decimal VolumeCapacity { get; set; }
+        public decimal OccupiedArea { get; }
+        public decimal OccupiedVolume { get; }
         protected StorageUnit() { }
-        protected StorageUnit(int id) { Id = id; }
-        public abstract bool CanStore(Goods goods);
+        protected StorageUnit(int id, decimal areaCapacity, decimal volumeCapacity) 
+        { 
+            Id = id;
+            AreaCapacity = areaCapacity;
+            VolumeCapacity = volumeCapacity;
+        }
+        public bool CanStore(Goods goods) => OccupiedArea + goods.TotalArea <= AreaCapacity || OccupiedVolume + goods.TotalVolume <= VolumeCapacity;
         public void AddGoods(Goods goods)
         {
             if (!CanStore(goods)) throw new InvalidOperationException("Недостаточно места");
