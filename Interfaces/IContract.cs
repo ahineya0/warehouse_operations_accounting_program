db86a689ee05b408cdb0f3ee88cca3c68505254b
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 17:03:41 +0700

    Added class Contract and interface IContract

---
+++ Interfaces/IContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Models;
+
+namespace warehouse_operations_accounting_program.Interfaces
+{
+    public interface IContract
+    {
+        bool IsActive();
+        bool IsPaid();
+        decimal CalculateTotalCost();
+    }
+}
---
+++ Models/Contract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Interfaces;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public class Contract : IContract
+    {
+        public IContractor Client { get; set; }
+        public IWarehouse Warehouse { get; set; }
+        public int RentedUnits { get; set; }
+        public decimal RatePerDay { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Payment Payment { get; set; }
+        public bool IsActive() => DateTime.Now <= EndDate;
+        public bool IsPaid() => Payment.Status == PaymentStatus.Paid;
+        public decimal CalculateTotalCost() => (EndDate - StartDate).Days * RentedUnits * RatePerDay;
+    }
+}
