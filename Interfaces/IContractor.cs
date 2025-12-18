b87cb5639aed17fb21dd1455dd04d4eda743660e
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 17:05:42 +0700

    Added class Contractor and interface IContractor

---
+++ Interfaces/IContractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Models;
+
+namespace warehouse_operations_accounting_program.Interfaces
+{
+    public interface IContractor
+    {
+        string Name { get; }
+        string ContactInfo { get; }
+        IReadOnlyList<IContract> Contracts { get; }
+        decimal CalculateTotalDebt();
+    }
+}
---
+++ Models/Contractor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Interfaces;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public abstract class Contractor : IContractor
+    {
+        protected readonly List<IContract> contracts = new();
+
+        public string Name { get; set; }
+        public string ContactInfo { get; set; }
+        public IReadOnlyList<IContract> Contracts => contracts;
+
+
+        protected Contractor() { }
+        protected Contractor(string name, string contact)
+        {
+            Name = name;
+            ContactInfo = contact;
+        }
+
+        public void AddContract(Contract contract) => contracts.Add(contract);
+
+        public decimal CalculateTotalDebt() => contracts.Where(c => c.IsActive() && !c.IsPaid()).Sum(c => c.CalculateTotalCost());
+    }
+}
