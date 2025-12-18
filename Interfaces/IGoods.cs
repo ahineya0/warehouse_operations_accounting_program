67012e06a5be4428d45b27f1965d5083ab65cdce
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 14:34:39 +0700

    Added interface IGoods

---
+++ Interfaces/IGoods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Interfaces
+{
+    public interface IGoods
+    {
+        decimal TotalArea();
+        decimal TotalVolume();
+    }
+}
--- Models/Goods.cs
+++ Models/Goods.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using warehouse_operations_accounting_program.Interfaces;
 
 namespace warehouse_operations_accounting_program.Models
 {
-    public class Goods
+    public class Goods : IGoods
     {
         public string Name { get; set; }
         public int Quantity { get; set; }
@@ -12,7 +13,7 @@ namespace warehouse_operations_accounting_program.Models
         public decimal VolumePerUnit { get; set; }
 
 
-        public decimal TotalArea => Quantity * AreaPerUnit;
-        public decimal TotalVolume => Quantity * VolumePerUnit;
+        public decimal TotalArea() => Quantity * AreaPerUnit;
+        public decimal TotalVolume() => Quantity * VolumePerUnit;
     }
 }
