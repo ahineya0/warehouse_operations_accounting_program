d4d45dfdd35f85fabcbe9b16608f6a49e2468e21
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 12:57:30 +0700

    Added class Document and interface IDocument

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Models;
+
+namespace warehouse_operations_accounting_program.Interfaces
+{
+    public interface IDocument
+    {
+        DateTime Date { get; }
+        IContractor Owner { get; }
+        IReadOnlyList<Goods> Goods { get; }
+
+        int GetTotalQuantity();
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using warehouse_operations_accounting_program.Interfaces;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public abstract class Document : IDocument
+    {
+        protected readonly List<Goods> goods = new();
+
+        public DateTime Date { get; }
+        public IContractor Owner { get; }
+
+        public IReadOnlyList<Goods> Goods => goods;
+
+        protected Document(DateTime date, IContractor owner)
+        {
+            Date = date;
+            Owner = owner;
+        }
+
+        public int GetTotalQuantity() => goods.Sum(g => g.Quantity);
+    }
+}
