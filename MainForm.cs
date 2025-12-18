2dc75fc4c46af4d5be7edcb9b0d7b409eb6ef600
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 11:45:20 +0700

    First commit + class Goods

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace warehouse_operations_accounting_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Models/Goods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public class Goods
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal AreaPerUnit { get; set; }
+        public decimal VolumePerUnit { get; set; }
+
+
+        public decimal TotalArea => Quantity * AreaPerUnit;
+        public decimal TotalVolume => Quantity * VolumePerUnit;
+    }
+}
