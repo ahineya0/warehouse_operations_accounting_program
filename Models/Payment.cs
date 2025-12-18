0cbef30bbc65b9c29ddd072939e3c67b30285cb1
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 11:52:32 +0700

    Added class Payment

---
+++ Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public enum PaymentStatus { Pending, Paid, Overdue }
+
+    public class Payment
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+    }
+}
