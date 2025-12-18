dba2985b994b80daa94db6e626b372f761bcf6b9
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 15:35:47 +0700

    New interface logic for Goods

---
+++ Interfaces/IGoodsInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Interfaces
+{
+    public interface IGoodsInformation
+    {
+        string Name { get; set; }
+        int Quantity { get; set; }
+    }
+}
--- Models/Goods.cs
+++ Models/Goods.cs
@@ -5,13 +5,22 @@ using warehouse_operations_accounting_program.Interfaces;
 
 namespace warehouse_operations_accounting_program.Models
 {
-    public class Goods : IGoods
+    public class Goods : IGoodsCharacteristics, IGoodsInformation
     {
         public string Name { get; set; }
+        public List<WarehouseType> RequiredStorageType { get; set; }
         public int Quantity { get; set; }
         public decimal AreaPerUnit { get; set; }
         public decimal VolumePerUnit { get; set; }
 
+        Goods(string name, List<WarehouseType> requiredStorageType, int quantity, decimal areaPerUnit, decimal volumePerUnit)
+        {
+            Name = name;
+            RequiredStorageType = requiredStorageType;
+            Quantity = quantity;
+            AreaPerUnit = areaPerUnit;
+            VolumePerUnit = volumePerUnit;
+        }
 
         public decimal TotalArea() => Quantity * AreaPerUnit;
         public decimal TotalVolume() => Quantity * VolumePerUnit;
