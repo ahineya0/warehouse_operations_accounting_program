6f1c6cbbb5cdbe95c0f398f24c151fa0baaf6242
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 15:06:21 +0700

    Added class User

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public enum UserRole
+    {
+        Operator,
+        Manager,
+        Accountant,
+        Client,
+        Administrator
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+        public bool IsBlocked { get; set; }
+    }
+}
