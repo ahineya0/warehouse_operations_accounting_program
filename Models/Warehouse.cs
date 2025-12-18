b2f70aebd98a05633a2529601e58384c7c1b496c
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 15:32:52 +0700

    New warehouse types logic + IWarehouse

--- Models/Warehouse.cs
+++ Models/Warehouse.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using warehouse_operations_accounting_program.Interfaces;
 
 namespace warehouse_operations_accounting_program.Models
 {
+    public enum WarehouseType { Construction, Refrigeration, Heating }
+
     public class Warehouse
     {
         public string Name { get; set; }
-        public string Type { get; set; }
+        public List<WarehouseType> Type { get; set; }
         public string Address { get; set; }
-        public List<StorageUnit> StorageUnits { get; set; } = new();
+        public List<IStorageUnit> StorageUnits { get; set; } = new();
         public Warehouse() { }
-        public Warehouse(string name, string type, string address)
+        public Warehouse(string name, List<WarehouseType> type, string address)
         {
             Name = name;
             Type = type;
             Address = address;
         }
-        public decimal FreeCapacity() => StorageUnits.Sum(s => s.Capacity - s.Occupied);
+        public decimal FreeAreaCapacity() => StorageUnits.Sum(s => s.FreeAreaCapacity());
+        public decimal FreeVolumeCapacity() => StorageUnits.Sum(s => s.FreeVolumeCapacity());
+        public bool TryStoreGoods(IGoods goods)
+        {
+            var unit = StorageUnits.FirstOrDefault(u => u.CanStore(goods));
+            if (unit == null) return false;
+
+            unit.AddGoods(goods);
+            return true;
+        }
     }
 }
