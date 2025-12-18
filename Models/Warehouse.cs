399be147ec58bdb838b594775788c05e67838871
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 12:39:02 +0700

    Added class Warehouse

---
+++ Models/Warehouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public class Warehouse
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public List<StorageUnit> StorageUnits { get; set; } = new();
+        public Warehouse() { }
+        public Warehouse(string name, string type, string address)
+        {
+            Name = name;
+            Type = type;
+            Address = address;
+        }
+        public decimal FreeCapacity() => StorageUnits.Sum(s => s.Capacity - s.Occupied);
+    }
+}
