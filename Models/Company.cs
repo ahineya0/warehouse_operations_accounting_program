55f001ece0333abd5310ea130d9ecc476f567ba4
ahineya0 <notheyarethesame@gmail.com>
2025-12-18 17:08:28 +0700

    Added 2 types of Contractor

---
+++ Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public class Company : Contractor
+    {
+        public string INN { get; set; }
+        public string KPP { get; set; }
+        public Company() { }
+        public Company(string name, string contact, string inn, string kpp) : base(name, contact)
+        {
+            INN = inn;
+            KPP = kpp;
+        }
+    }
+}
---
+++ Models/Individual.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace warehouse_operations_accounting_program.Models
+{
+    public class Individual : Contractor
+    {
+        public string PassportData { get; set; }
+        public Individual() { }
+        public Individual(string name, string contact, string passport) : base(name, contact)
+        {
+            PassportData = passport;
+        }
+        public override string GetContractorType() => "Физическое лицо";
+    }
+}
